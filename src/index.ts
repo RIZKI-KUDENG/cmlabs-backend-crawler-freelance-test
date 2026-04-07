18cefbd0e6ed6ef178820764011d2ee31abcb99e
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-04-07 14:16:11 +0700

    testing PWA

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { crawl } from "./crawler";
 
-async function main(){
-    await crawl('https://cmlabs.co', 'cmlabs.html');
-  await crawl('https://sequence.day', 'sequence.html');
+async function main() {
+  await crawl("https://cmlabs.co", "cmlabs.html");
+  await crawl("https://sequence.day", "sequence.html");
+  await crawl("https://www.pinterest.com", "pinterest.html");
 }
 
 main();
